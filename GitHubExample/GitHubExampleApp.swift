80ff26b2fa3caf7b3956cad35bde828c00494106
earthtonaila <surahmatnaila@gmail.com>
2024-06-25 09:44:51 -0700

    Initial Commit

---
+++ GitHubExample/GitHubExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubExampleApp.swift
+//  GitHubExample
+//
+//  Created by Scholar on 6/25/24.
+//
+
+import SwiftUI
+
+@main
+struct GitHubExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
